d8375b1aff7a6bb5a42572abe578873d49a7ab91
djamal <djamaldine-mohamed@live.fr>
2023-03-28 10:21:44 -0700

    Création de tous les fichiers

--- main.h
+++ main.h
@@ -1,7 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+typedef struct {
+char type;
+int (*func)(va_list);
+} print_handler;
+
+int handle_format_specifier(char specifier, va_list args);
 int _printf(const char *format, ...);
-int _putchar(char c);
+int print_char(va_list args);
+int print_str(va_list args);
+int print_percent(va_list args);
+int print_int(va_list args);
 
 #endif
