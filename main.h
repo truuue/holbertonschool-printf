82464fad0471c454102d988e2ee427c38b2feb99
djamal <djamaldine-mohamed@live.fr>
2023-03-29 16:32:43 -0700

    fixe all betty error

--- main.h
+++ main.h
@@ -6,23 +6,32 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+/**
+ * struct print_handler - handles printing functions
+ * @type: the type of print function
+ * @func: a pointer to the print function
+ */
 
-typedef struct {
-    char type;
-    int (*func)(va_list);
+typedef struct print_handler
+{
+char type;
+int (*func)(va_list);
 } print_handler;
 
 
-
+/* printf.c */
 
 int _printf(const char *format, ...);
 
+/* printers */
 
 int print_char(va_list args);
 int print_string(va_list args);
 int print_percent(va_list args);
 int print_int(va_list list);
 int _putchar(char c);
+
+/* handlers */
 
 int handle_format_specifier(char specifier, va_list args);
 
